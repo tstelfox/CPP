cfc625f9d9bcbd8586bf83994be402d4f5ef5b6a
Turlough stelfox Mullan <duke1199sp@gmail.com>
2020-11-15 19:49:54 +0100

    gerbur taim

--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/15 18:55:31 by tmullan       #+#    #+#                 */
-/*   Updated: 2020/11/15 19:43:40 by tmullan       ########   odam.nl         */
+/*   Updated: 2020/11/15 19:49:27 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int		main() {
 	std::cout << "Welcome to Turlough's phonebook, please type one of the following commands:" << std::endl;
 	std::cout << "ADD to add a user\nSEARCH to search the phonebook\nEXIT to quit the phonebook:\n";
 	std::cin >> buff;
-	std::cout << "OK! Let's [" << buff << "]" << std::endl;
-
+	if (buff.compare("ADD") == 0)
+		std::cout << "OK! Let's [" << buff << "]" << std::endl;
+	else
+		std::cout << "YOU WROTE SOMETHING FUCKED YOU DICKHEAD" << std::endl;
 
 	return (0);
 }
