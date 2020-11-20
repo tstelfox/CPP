1a863a9904ea6de2dc64872eef580b663c6ad661
Turlough Mullan <duke1199sp@gmail.com>
2020-11-20 15:49:05 +0100

    changed compiler to clang++

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/13 16:18:14 by tmullan       #+#    #+#                 */
-/*   Updated: 2020/11/13 17:20:56 by tmullan       ########   odam.nl         */
+/*   Updated: 2020/11/20 15:21:33 by tmullan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main(int argc, char **argv)
 	}
 	for (int i = 1; argv[i]; i++)
 	{
-		for (size_t k = 0; k < strlen(argv[i]); k++)
+		for (size_t k = 0; k < argv[i].size(); k++)
 			putchar(toupper(argv[i][k]));
 	}
 	std::cout << std::endl;
