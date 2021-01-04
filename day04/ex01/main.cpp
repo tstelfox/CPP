55094ad7464afcd2f996d365d2de48ccef765cdf
Codemuncher <codemuncher@iMac-di-logos.fritz.box>
2021-01-04 10:43:41 +0100

    Back to work codemunchers

--- day04/ex01/main.cpp
+++ day04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/20 21:00:15 by tmullan       #+#    #+#                 */
-/*   Updated: 2020/12/20 21:15:13 by tmullan       ########   odam.nl         */
+/*   Updated: 2021/01/04 10:27:54 by codemuncher   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,21 @@
 #include "RadScorpion.hpp"
 #include "SuperMutant.hpp"
 
-int		main(void) { //Need to test for more shit probably
+int		main(void) {
 
 	Character* me = new Character("me"); 
 	std::cout << *me;
 	Enemy* b = new RadScorpion();
 	AWeapon* pr = new PlasmaRifle(); 
 	AWeapon* pf = new PowerFist();
-	me->equip(pr); std::cout << *me; 
+	me->equip(pr);
+	std::cout << *me; 
 	me->equip(pf);
 	me->attack(b);
-	std::cout << *me; me->equip(pr); 
-	std::cout << *me; me->attack(b); 
+	std::cout << *me;
+	me->equip(pr); 
+	std::cout << *me;
+	me->attack(b); 
 	std::cout << *me;
 	me->attack(b); std::cout << *me;
 	return 0;
