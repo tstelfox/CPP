44b5eb96a106d72e33ac932d2483a52fa7127eef
Codemuncher <duke1199sp@gmail.com>
2021-01-12 16:59:08 +0100

    Seems to work

--- day04/ex03/main.cpp
+++ day04/ex03/main.cpp
@@ -16,20 +16,35 @@
 #include "Cure.hpp"
 
 int main() {
-	IMateriaSource* src = new MateriaSource(); 
-	src->learnMateria(new Ice()); 
-	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice"); 
-	me->equip(tmp);
-	tmp = src->createMateria("cure"); 
-	me->equip(tmp);
-	ICharacter* bob = new Character("bob");
-	me->use(0, *bob); 
-	me->use(1, *bob);
-	delete bob;
-	delete me;
-	delete src;
-	return 0;
+		{
+			IMateriaSource* src = new MateriaSource(); 
+			src->learnMateria(new Ice()); 
+			src->learnMateria(new Cure());
+			ICharacter* me = new Character("me");
+			AMateria* tmp;
+			tmp = src->createMateria("ice"); 
+			me->equip(tmp);
+			tmp = src->createMateria("cure"); 
+			me->equip(tmp);
+			ICharacter* bob = new Character("bob");
+			me->use(0, *bob); 
+			tmp = src->createMateria("cure"); 
+			me->equip(tmp);
+			tmp = src->createMateria("ice"); 
+			me->equip(tmp);
+			me->use(1, *bob);
+			me->use(2, *bob); 
+			me->use(3, *bob);
+			tmp = src->createMateria("cure"); 
+			me->equip(tmp);
+			me->use(3, *bob);
+
+
+			
+			delete bob;
+			delete me;
+			delete src;
+		}
+		system("leaks pprogg");
+		return 0;
 	}
