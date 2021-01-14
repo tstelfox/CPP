aa072d80ade599d301c72fd7cdfc0e0d363f6b5f
Turlough Mullan <duke1199sp@gmail.com>
2021-01-14 14:43:20 +0100

    day 05 must begin

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/14 14:30:34 by tmullan       #+#    #+#                 */
+/*   Updated: 2021/01/14 14:40:28 by tmullan       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+class Bureaucrat {
+
+	public:
+		name;
+		grade;
+};
+
+
+
+
+#endif
