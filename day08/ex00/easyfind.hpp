5da8c50643ee5bd3fcc551e3537fb524ba4c122d
Turlough stelfox Mullan <duke1199sp@gmail.com>
2021-08-04 16:38:51 +0200

    Starting work and reading up on STL

---
+++ day08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   easyfind.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/08/04 15:35:31 by tmullan       #+#    #+#                 */
+/*   Updated: 2021/08/04 16:05:03 by tmullan       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <string>
+
+tempate < typename T>
+void	easyfind(T const first, int const second)
+{
+	
+}
+
+
+#endif
