49fa51c2e142226c83c0c8068c2b0d53f374c01b
Turlough Mullan <duke1199sp@gmail.com>
2021-09-08 13:01:37 +0200

    Starting the class

---
+++ day08/ex02/mutantstack.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   mutantstack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tmullan <tmullan@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/09/08 12:39:32 by tmullan       #+#    #+#                 */
+/*   Updated: 2021/09/08 12:56:48 by tmullan       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+# include <stack>
+# include <algorithm>
+
+class MutantStack {
+	private:
+		/* data */
+	public:
+		MutantStack(/* args */);
+		~MutantStack();
+};
+
+MutantStack::MutantStack(/* args */)
+{
+}
+
+MutantStack::~MutantStack()
+{
+}
+
+
+#endif
